6bc5c8c7a3ce32318e587151e7cddf60c1e16443
Kelsie Bridge <kelsiebridge@mail.weber.edu>
2026-06-12 16:52:34 -0600

    fix: updated python code to match pylint results

--- script.py
+++ script.py
@@ -3,8 +3,8 @@
 # Sources:
 ## https://stackoverflow.com/questions/76082808/how-to-get-github-repo-latest-release-in-python
 
+from datetime import datetime, timezone
 import requests
-from datetime import datetime, timedelta, timezone
 from flask import Flask
 from github import Github
 
@@ -13,40 +13,44 @@ app = Flask(__name__)
 @app.route("/temperature")
 def temperature():
     """Get sensebox data and return average temperature from the last hour"""
-    
+
     # Ids for senseboxes, given by tutorial
     ids = ['5eba5fbad46fb8001b799786', '5c21ff8f919bf8001adf2488', '5ade1acf223bd80019a1011c']
     # Total temperature from the senseboxes, start at 0 degrees
     total = 0
     # Get the current time
     cur_time = datetime.now(timezone.utc)
-    
+
     # For each of the given boxes:
     for box_id in ids:
         # Get json results from API
         content = requests.get(f'https://api.opensensemap.org/boxes/{box_id}?format=json')
         # Check for successful connection
-        if (content.status_code == 200):
+        if content.status_code == 200:
             # Get all sensors from sensebox
             sensors = content.json()['sensors']
             # Loop through each of the sensors
             for sensor in sensors: 
                 if sensor['title'] == 'Temperatur':
-                    # If there is not a last measurement, or a date or value for that measurement, return and alert user
-                    if sensor['lastMeasurement'] is None or sensor['lastMeasurement']['createdAt'] is None or sensor['lastMeasurement']['value'] is None:
+                    # If there is no last measurement, or date or value for the measurement, return and alert
+                    result1 = sensor['lastMeasurement']
+                    result2 = sensor['lastMeasurement']['createdAt']
+                    result3 = sensor['lastMeasurement']['value']
+                    if result1 is None or result2 is None or result3 is None:
                         return "No last measurement"
                     # See if last measurement was within the last hour
-                    time_diff = cur_time - datetime.fromisoformat(sensor['lastMeasurement']['createdAt'])
+                    measurement_time = datetime.fromisoformat(sensor['lastMeasurement']['createdAt'])
+                    time_diff = cur_time - measurement_time
                     recent = time_diff.total_seconds() < 3600
                     # If there is a temperature sensor, add its value to the sum
                     if recent:
                         total += float(sensor['lastMeasurement']['value'])
         else: # If connection failed, return and alert user
             return "Could not connect to API"
-    
+
     # Divide the sum of all the temperatures by 3 to get the average
     avg = total/3
-    
+
     # Return the sum and average
     return {"totaltemp": total, "averagetemp": avg}
 
